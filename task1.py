b2714526be03155161c6bc1292add65bfd015510
Denver <denverm@yandex.ru>
2023-07-01 19:10:37 +0300

    Add task1

---
+++ task1.py
@@ -0,0 +1,20 @@
+a = 5
+print(a, type(a))
+
+b = 2.68
+print(b, type(b))
+
+c = "Hello"
+print(c, type(c))
+
+d = [1, 2, 3]
+print(d, type(d))
+
+e = (1, 2)
+print(e, type(e))
+
+f = (5==5)
+print(f, type(f))
+
+g = {1, 2, 3}
+print(g, type(g))
